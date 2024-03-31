be8517cd916d0474f70d21d4ede12bc8ac21899f
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-03-31 21:21:30 +0200

    Rozbij i dodaj metody printowe

--- backend/src/dbinit.py
+++ backend/src/dbinit.py
@@ -1,6 +1,7 @@
 from track.infrastructure.persistence.database import engine
 from track.application.models.base import Base
-from track.application.models.library import TrackModel, QueuedTrackModel  # noqa
+from track.application.models.library import TrackModel  # noqa
+from track.application.models.queue import QueuedTrackModel  # noqa
 
 # https://stackoverflow.com/a/73639613/14011471
 # turbo dziwne, ale trzeba zaimportować te modele, by się stworzyły
