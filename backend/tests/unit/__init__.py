7f9eb8638d2b0874c7853489b802ee8ee01dc1ba
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-05-28 21:54:28 +0200

    Przenieś testy do src/

--- backend/tests/unit/__init__.py
+++
@@ -1,7 +0,0 @@
-from kink import di
-from src.track.application.interfaces.youtube_api import YoutubeAPIInterface
-from src.track.infrastructure.youtube_api import YoutubeAPI
-
-
-def boostrap_di() -> None:
-    di[YoutubeAPIInterface] = YoutubeAPI
