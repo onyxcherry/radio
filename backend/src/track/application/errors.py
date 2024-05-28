731c182ed2107fded135f4690c8f77429cd75027
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-05-28 21:57:32 +0200

    Dorzuć errory API

---
+++ backend/src/track/application/errors.py
@@ -0,0 +1,18 @@
+class TrackAPIConfigurationError(Exception):
+    pass
+
+
+class TrackAPIMalformedResponse(Exception):
+    pass
+
+
+class TrackAPINoResults(Exception):
+    pass
+
+
+class TrackAPITimeout(Exception):
+    pass
+
+
+class TrackAPIConnectionError(Exception):
+    pass
