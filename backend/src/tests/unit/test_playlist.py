96abeb8a273305238285289f454935fde9759aac
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-05 21:13:18 +0200

    Użyj di w playlist test

--- backend/src/tests/unit/test_playlist.py
+++ backend/src/tests/unit/test_playlist.py
@@ -1,51 +1,48 @@
-from datetime import timedelta
 from kink import di
 from pytest import fixture
-from building_blocks.clock import Clock
-from track.domain.breaks import Breaks, PlayingTime
+from track.application.library import Library
 from track.domain.entities import TrackRequested
 from track.application.playlist import Playlist
-from track.infrastructure.inmemory_playlist_repository import InMemoryPlaylistRepository
-from .data import TRACKS
+from .data import FUTURE_PT, TRACKS
 
-playlist_repo = InMemoryPlaylistRepository()
-playlist = Playlist(playlist_repo)
-clock = di[Clock]
-
-track = TRACKS[0]
-tomorrow_date = clock.get_current_date() + timedelta(days=1)
-playing_time = PlayingTime(tomorrow_date, Breaks.SECOND)
-requested = TrackRequested(track.identity, playing_time)
+playlist = di[Playlist]
+library = di[Library]
 
 
 @fixture(autouse=True)
 def reset():
+    playlist_repo = playlist._playlist_repository
+    library_repo = library._library_repository
     playlist_repo.delete_all()
+    library_repo.delete_all()
+
+    library_repo.add(TRACKS[0])
 
     yield
 
     playlist_repo.delete_all()
+    library_repo.delete_all()
 
 
 # def test_gets_tracks_only_not_played():
 
 
 def test_adds_track_to_playlist():
-    track = TRACKS[0]
-    tomorrow_date = clock.get_current_date() + timedelta(days=1)
-    playing_time = PlayingTime(tomorrow_date, Breaks.SECOND)
-    request = TrackRequested(track.identity, playing_time)
+    playing_time = FUTURE_PT
+    requested = TrackRequested(TRACKS[0].identity, playing_time)
 
-    playlist.add(request)
+    playlist.add(requested)
 
     playlist_tracks_list = playlist.get_all(playing_time.date_)
 
     track_queued = playlist_tracks_list[0]
-    assert track_queued.identity == track.identity
+    assert track_queued.identity == requested.identity
     assert track_queued.when == playing_time
 
 
 def test_marks_as_played():
+    playing_time = FUTURE_PT
+    requested = TrackRequested(TRACKS[0].identity, playing_time)
     playlist.add(requested)
 
     track = playlist.get(
