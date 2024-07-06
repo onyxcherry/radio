bac06737749b02540a5c65255d763eceeea3764c
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-06 21:55:32 +0200

    Use real objects for production run

--- backend/src/track/boostrap.py
+++ backend/src/track/boostrap.py
@@ -1,26 +1,26 @@
 from kink import di
+from track.application.library import Library
+from track.application.playlist import Playlist
+from track.domain.providers.youtube import YoutubeTrackProvided
+from track.infrastructure.db_library_repository import DBLibraryRepository
+from track.infrastructure.db_playlist_repository import DBPlaylistRepository
 from building_blocks.clock import Clock, SystemClock
 from track.application.interfaces.youtube_api import YoutubeAPIInterface
 from track.infrastructure.youtube_api import YoutubeAPI
 from track.application.requests_service import RequestsService
-from track.domain.library_repository import LibraryRepository
-from track.domain.playlist_repository import PlaylistRepository
-
-from track.infrastructure.inmemory_library_repository import (
-    InMemoryLibraryRepository,
-)
-from track.infrastructure.inmemory_playlist_repository import (
-    InMemoryPlaylistRepository,
-)
 
 
 def boostrap_di() -> None:
-    di[LibraryRepository] = InMemoryLibraryRepository
-    di[PlaylistRepository] = InMemoryPlaylistRepository
+    real_library_repo = DBLibraryRepository()
+    real_playlist_repo = DBPlaylistRepository()
+    system_clock = SystemClock()
     di[RequestsService] = RequestsService(
-        InMemoryLibraryRepository(),
-        InMemoryPlaylistRepository(),
-        SystemClock(),
+        real_library_repo,
+        real_playlist_repo,
+        system_clock,
     )
-    di[YoutubeAPIInterface] = YoutubeAPI
-    di[Clock] = SystemClock()
+    di[Library] = Library(real_library_repo)
+    di[Playlist] = Playlist(real_playlist_repo)
+    di[YoutubeAPIInterface] = YoutubeAPI()
+    di[YoutubeTrackProvided] = YoutubeAPI
+    di[Clock] = system_clock
