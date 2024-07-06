ad5b4a56a3737e0b8dbb41f8fcfeae0ec246672d
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-06 21:14:40 +0200

    Move dbinit file

--- backend/src/track/infrastructure/persistence/dbinit.py
+++ backend/src/track/infrastructure/persistence/dbinit.py
@@ -1,10 +1,9 @@
 from track.infrastructure.persistence.database import engine
 from track.application.models.base import Base
-from track.application.models.library import TrackModel  # noqa
-from track.application.models.queue import QueuedTrackModel  # noqa
+from track.application.models.library import LibraryTrackModel  # noqa
+from track.application.models.queue import QueueTrackModel  # noqa
 
 # https://stackoverflow.com/a/73639613/14011471
-# turbo dziwne, ale trzeba zaimportować te modele, by się stworzyły
 
 Base.metadata.create_all(bind=engine)
 print(Base.metadata)
