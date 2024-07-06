f09c0a3325a2bd04427c3fd2fb3e2f6581703316
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-06 21:12:40 +0200

    Clean bootstrapping process for tests

--- backend/src/tests/bootstrap.py
+++ backend/src/tests/bootstrap.py
@@ -1,4 +1,7 @@
 from kink import di
+from track.domain.library_repository import LibraryRepository
+from track.infrastructure.db_library_repository import DBLibraryRepository
+from track.infrastructure.db_playlist_repository import DBPlaylistRepository
 from track.application.playlist import Playlist
 from track.application.library import Library
 from track.domain.providers.youtube import YoutubeTrackProvided
@@ -15,17 +18,23 @@ from track.application.interfaces.youtube_api import YoutubeAPIInterface
 
 
 def bootstrap_di() -> None:
-    inmemory_library_repo = InMemoryLibraryRepository()
-    inmemory_playlist_repo = InMemoryPlaylistRepository()
+    # inmemory_library_repo = InMemoryLibraryRepository()
+    # inmemory_playlist_repo = InMemoryPlaylistRepository()
+    real_library_repo = DBLibraryRepository()
+    real_playlist_repo = DBPlaylistRepository()
     system_clock = SystemClock()
-
+    di[LibraryRepository] = real_library_repo
     di[RequestsService] = RequestsService(
-        inmemory_library_repo,
-        inmemory_playlist_repo,
+        # inmemory_library_repo,
+        # inmemory_playlist_repo,
+        real_library_repo,
+        real_playlist_repo,
         system_clock,
     )
-    di[Library] = Library(inmemory_library_repo)
-    di[Playlist] = Playlist(inmemory_playlist_repo)
+    # di[Library] = Library(inmemory_library_repo)
+    # di[Playlist] = Playlist(inmemory_playlist_repo)
+    di[Library] = Library(real_library_repo)
+    di[Playlist] = Playlist(real_playlist_repo)
     di[YoutubeAPIInterface] = InMemoryYoutubeAPI()
     di[YoutubeTrackProvided] = InMemoryYoutubeAPI
     di[Clock] = system_clock
