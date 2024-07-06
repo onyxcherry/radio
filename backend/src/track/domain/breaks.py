59b7d7f2df5239e823f975703a55579e30162d0e
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-06 19:20:43 +0200

    Use Seconds for breaks durations

--- backend/src/track/domain/breaks.py
+++ backend/src/track/domain/breaks.py
@@ -2,16 +2,18 @@ from dataclasses import dataclass
 from enum import IntEnum, auto, unique
 from datetime import date, datetime, time, timezone
 
+from track.domain.provided import Seconds
+
 
 _BREAKS_START_TIMES = {
-    time(8, 30): 10,
-    time(9, 25): 10,
-    time(10, 20): 10,
-    time(11, 15): 15,
-    time(12, 15): 10,
-    time(13, 10): 10,
-    time(14, 5): 10,
-    time(15, 00): 10,
+    time(8, 30): Seconds(10 * 60),
+    time(9, 25): Seconds(10 * 60),
+    time(10, 20): Seconds(10 * 60),
+    time(11, 15): Seconds(15 * 60),
+    time(12, 15): Seconds(10 * 60),
+    time(13, 10): Seconds(10 * 60),
+    time(14, 5): Seconds(10 * 60),
+    time(15, 00): Seconds(10 * 60),
 }
 
 
