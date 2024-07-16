b0ce465f33f975bf1110edccc9948eb5b7ff86a3
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-16 22:39:50 +0200

    Add inmemory events producer and consumer impls

---
+++ backend/src/track/infrastructure/messaging/inmemory_events_producer.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from track.application.interfaces.events import (
+    EventsProducer,
+    MessagesOptions,
+    ProducerConnectionOptions,
+)
+from track.domain.events.base import Event
+from track.infrastructure.messaging.schema_utils import SchemaRegistryConfig
+
+
+class InMemoryEventsProducer(EventsProducer):
+    def __init__(
+        self,
+        conn_options: Optional[ProducerConnectionOptions] = None,
+        msg_options: Optional[MessagesOptions] = None,
+        schema_config: Optional[SchemaRegistryConfig] = None,
+        test: bool = False,
+    ) -> None:
+        # topic name -> messages
+        self._messages: dict[str, list[Event]] = {}
+
+    def produce(self, topic: str, message: Event) -> None:
+        if topic not in self._messages:
+            self._messages[topic] = []
+        self._messages[topic].append(message)
