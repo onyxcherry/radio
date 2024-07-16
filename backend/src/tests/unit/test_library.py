c73732dd0f38b10ae27ff26bb861cbe2f7daad86
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-16 22:56:17 +0200

    Assert produced events

--- backend/src/tests/unit/test_library.py
+++ backend/src/tests/unit/test_library.py
@@ -1,11 +1,26 @@
+from datetime import datetime
 from kink import di
 from pytest import fixture
+from building_blocks.clock import Clock
+from track.domain.events.library import (
+    TrackAccepted,
+    TrackAddedToLibrary,
+    TrackRejected,
+)
+from track.domain.provided import Identifier, TrackProvidedIdentity
+from track.application.interfaces.events import EventsConsumer
 from tests.unit.data import ACCEPTED_TRACKS, NEW_TRACKS, PENDING_APPROVAL_TRACKS
 from track.application.library import Library
 from track.domain.entities import Status
 
 
 library = di[Library]
+events_consumer = di[EventsConsumer]
+events_consumer.subscribe(library._events_topic)
+events_producer = library._events_producer
+clock = di[Clock]
+
+fixed_dt = datetime(2024, 7, 16, 14, 19, 21)
 
 
 @fixture(autouse=True)
@@ -31,6 +46,9 @@ def test_new_track_has_pending_approval_state():
     got_track = library.get(track.identity)
     assert got_track is not None
     assert got_track.status == Status.PENDING_APPROVAL
+    sync_messages()
+    expected_event = TrackAddedToLibrary(identity=track.identity, created=fixed_dt)
+    assert expected_event in events_consumer.consume(10)
 
 
 def test_accept_track(tracks_one_accepted):
@@ -40,6 +58,13 @@ def test_accept_track(tracks_one_accepted):
     got_track = library.get(track.identity)
     assert got_track is not None
     assert got_track.status == Status.ACCEPTED
+    sync_messages()
+    expected_event = TrackAccepted(
+        identity=track.identity,
+        previous_status=Status.PENDING_APPROVAL,
+        created=fixed_dt,
+    )
+    assert expected_event in events_consumer.consume(10)
 
 
 def test_reject_track(tracks_one_accepted):
@@ -50,6 +75,13 @@ def test_reject_track(tracks_one_accepted):
     got_track = library.get(track.identity)
     assert got_track is not None
     assert got_track.status == Status.REJECTED
+    sync_messages()
+    expected_event = TrackRejected(
+        identity=track.identity,
+        previous_status=Status.PENDING_APPROVAL,
+        created=fixed_dt,
+    )
+    assert expected_event in events_consumer.consume(10)
 
 
 def test_filters_tracks_by_status(tracks_one_accepted):
