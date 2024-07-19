5a853295a16173a55b0cdaa5d46766ed3e0fd2bb
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-19 22:58:04 +0200

    More fixes

--- backend/src/track/infrastructure/messaging/inmemory_events_producer.py
+++ backend/src/track/infrastructure/messaging/inmemory_events_producer.py
@@ -18,5 +18,11 @@ class InMemoryEventsProducer(EventsProducer):
     ) -> None:
         # topic name -> messages
         self._messages: dict[str, list[Event]] = {}
+        self._schema_config = schema_config
         self._topic_name = self._schema_config.topic_name
+        self._messages[self._topic_name] = []
+
     def produce(self, message: Event) -> None:
+        if self._topic_name not in self._messages:
+            self._messages[self._topic_name] = []
+        self._messages[self._topic_name].append(message)
