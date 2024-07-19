50f2a7179b340759856eee41cd2906504471a6e1
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-07-19 22:59:02 +0200

    Change inner implementation of Kafka producers and consumers classes

--- backend/src/track/infrastructure/messaging/kafka_events_producer.py
+++ backend/src/track/infrastructure/messaging/kafka_events_producer.py
@@ -1,24 +1,22 @@
 from datetime import date
+from typing import Literal, Optional
 from uuid import uuid4
 
 from confluent_kafka import Producer
 from track.domain.events.base import Event
 from track.infrastructure.messaging.schema_utils import (
     SchemaRegistryConfig,
+    create_client,
     fetch_schema,
 )
 from confluent_kafka.schema_registry.avro import AvroSerializer
 
 from track.application.interfaces.events import (
     EventsProducer,
-    MessagesOptions,
     ProducerConnectionOptions,
+    ProducerMessagesOptions,
 )
-from confluent_kafka.serialization import (
-    StringSerializer,
-    SerializationContext,
-    MessageField,
-)
+from confluent_kafka.serialization import SerializationContext, MessageField
 
 
 def delivery_report(err, msg):
@@ -60,40 +58,53 @@ class KafkaAvroEventsProducer(EventsProducer):
     def __init__(
         self,
         conn_options: ProducerConnectionOptions,
-        msg_options: MessagesOptions,
+        msg_options: ProducerMessagesOptions,
         schema_config: SchemaRegistryConfig,
         test: bool = False,
     ) -> None:
         producer_conf = {
             "bootstrap.servers": conn_options.bootstrap_servers,
             "client.id": conn_options.client_id,
-            "auto.offset.reset": "earliest",
         }
         self._producer_conf = producer_conf
-        producer = Producer(producer_conf)
-        self._producer = producer
+        self._producer = Producer(producer_conf)
+        self._partitioner = msg_options.partitioner
+        self._key_serializer = msg_options.key_serializer
+        self._value_serializer = msg_options.value_serializer
         self._schema_config = schema_config
-        self._string_serializer = StringSerializer("utf_8")
-        self._connect_schema_reg()
-        self._avro_serializer = self._connect_schema_reg()
+        self._schema_reg_client = create_client(self._schema_config)
+        self._avro_serializer = self._create_avro_serializer(
+            schema_id=schema_config.schema_id, subject_name=schema_config.subject_name
+        )
 
-    def _connect_schema_reg(self) -> AvroSerializer:
+    def _create_avro_serializer(
+        self, schema_id: int | Literal["latest"], subject_name: Optional[str]
+    ) -> AvroSerializer:
         conf = {"auto.register.schemas": False}
-        schema_reg_client, schema_str = fetch_schema(self._schema_config)
+        schema_str = fetch_schema(
+            client=self._schema_reg_client,
+            schema_id=schema_id,
+            subject_name=subject_name,
+        )
         avro_serializer = AvroSerializer(
-            schema_reg_client, schema_str=schema_str, conf=conf
+            self._schema_reg_client,
+            schema_str=schema_str,
+            conf=conf,
+            to_dict=json_serial,
         )
         return avro_serializer
 
     def produce(self, message: Event) -> None:
         key = self._key_serializer(str(uuid4()))
+        topic = self._schema_config.topic_name
+        value = self._avro_serializer(
+            message, SerializationContext(topic, MessageField.VALUE)
+        )
         try:
             self._producer.produce(
                 topic=topic,
                 key=key,
-                value=self._avro_serializer(
-                    message, SerializationContext(topic, MessageField.VALUE)
-                ),
+                value=value,
                 on_delivery=delivery_report,
             )
         except ValueError as ex:
