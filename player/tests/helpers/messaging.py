c6824a3f1da2eab034a0d129a02bbc8d1e14130f
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-08-23 17:24:24 +0200

    Use faster way to reset Kafka partition offset

--- player/tests/helpers/messaging.py
+++ player/tests/helpers/messaging.py
@@ -1,4 +1,6 @@
+import json
 import subprocess
+import tempfile
 from kink import di
 from player.src.infrastructure.messaging.inmemory_events_helper import InMemoryEvents
 from player.tests.choices import DIChoices
@@ -7,11 +9,18 @@ from player.tests.choices import DIChoices
 def reset_events() -> None:
     di_choices = di[DIChoices]
     if di_choices.real_msg_broker:
-        command = "/home/tomasz/.local/bin/rpk -X brokers=127.0.0.1:19092 topic trim-prefix queue -o end --no-confirm"
-        result = subprocess.run(
-            command, shell=True, check=False, text=True, capture_output=True
-        )
-        # ./kafka-delete-records.sh --bootstrap-server localhost:19092 --offset-json-file ~/delete-records.json
-        # {"partitions": [{"topic": "queue", "partition": 0, "offset": 1}], "version": 1}
+        data = {
+            "partitions": [{"topic": "queue", "partition": 0, "offset": 58881}],
+            "version": 1,
+        }
+        with tempfile.NamedTemporaryFile(
+            mode="w+", suffix=".json", delete_on_close=False
+        ) as fp:
+            json.dump(data, fp)
+            fp.close()
+            command = f"/home/tomasz/kafka_2.13-3.7.1/bin/kafka-run-class.sh org.apache.kafka.tools.DeleteRecordsCommand --bootstrap-server localhost:19092 --offset-json-file {fp.name}"
+            result = subprocess.run(
+                command, shell=True, check=False, text=True, capture_output=True
+            )
     else:
         di[InMemoryEvents].reset()
