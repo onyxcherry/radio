ec68e985638eebd7a5953f5ccb8d27c1b44e15f1
Tomasz Wiśniewski <tomasz@wisniewski.app>
2024-09-01 18:49:36 +0200

    Improve reset offsets function

--- player/tests/helpers/messaging.py
+++ player/tests/helpers/messaging.py
@@ -6,21 +6,35 @@ from player.src.infrastructure.messaging.inmemory_events_helper import InMemoryE
 from player.tests.choices import DIChoices
 
 
+def reset_offset(topic: str) -> None:
+    kafka_run_class_sh_path = "/home/tomasz/kafka_2.13-3.7.1/bin/kafka-run-class.sh"
+    bootstrap_server = "localhost:19092"
+    get_offset_command = f"{kafka_run_class_sh_path} org.apache.kafka.tools.GetOffsetShell --bootstrap-server {bootstrap_server} --topic {topic}"
+    get_offset_result = subprocess.run(
+        get_offset_command, shell=True, check=True, text=True, capture_output=True
+    )
+    got_offset = int(get_offset_result.stdout.split(":")[-1])
+
+    data = {
+        "partitions": [{"topic": topic, "partition": 0, "offset": got_offset}],
+        "version": 1,
+    }
+    with tempfile.NamedTemporaryFile(
+        mode="w+", suffix=".json", delete_on_close=False
+    ) as fp:
+        delete_command = f"{kafka_run_class_sh_path} org.apache.kafka.tools.DeleteRecordsCommand --bootstrap-server {bootstrap_server} --offset-json-file {fp.name}"
+        json.dump(data, fp)
+        fp.close()
+        delete_result = subprocess.run(
+            delete_command, shell=True, check=False, text=True, capture_output=True
+        )
+        # assert "OffsetOutOfRangeException" not in delete_result.stdout
+
 def reset_events() -> None:
     di_choices = di[DIChoices]
     if di_choices.real_msg_broker:
-        data = {
-            "partitions": [{"topic": "queue", "partition": 0, "offset": 58881}],
-            "version": 1,
-        }
-        with tempfile.NamedTemporaryFile(
-            mode="w+", suffix=".json", delete_on_close=False
-        ) as fp:
-            json.dump(data, fp)
-            fp.close()
-            command = f"/home/tomasz/kafka_2.13-3.7.1/bin/kafka-run-class.sh org.apache.kafka.tools.DeleteRecordsCommand --bootstrap-server localhost:19092 --offset-json-file {fp.name}"
-            result = subprocess.run(
-                command, shell=True, check=False, text=True, capture_output=True
-            )
+        topics = ["queue", "library"]
+        for topic in topics:
+            reset_offset(topic)
     else:
         di[InMemoryEvents].reset()
