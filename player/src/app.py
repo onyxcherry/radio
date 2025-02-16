ec339cf782175863ff68cb410f7db24966281675
Tomasz Wiśniewski <tomasz@wisniewski.app>
2025-02-16 19:23:10 +0100

    Handle exceptions in tasks in app

--- player/src/app.py
+++ player/src/app.py
@@ -16,6 +16,40 @@ from player.src.infrastructure.messaging.types import PlaylistEventsConsumer
 logger = get_logger(__name__)
 
 
+class SubTaskFailed(RuntimeError):
+    pass
+
+
+async def manage_playing_with_exc(playing_manager: PlayingManager) -> None:
+    try:
+        await playing_manager.manage_playing()
+    except asyncio.CancelledError as err:
+        logger.info("Handling CancelledError")
+        playing_manager.handle_playing_immediate_stop()
+        raise asyncio.CancelledError from err
+
+
+async def update_current_break_with_exc(
+    break_observer: BreakObserver, playing_manager: PlayingManager
+) -> None:
+    try:
+        await break_observer.update_current_break()
+    except asyncio.CancelledError as err:
+        logger.info("Handling CancelledError")
+        playing_manager.handle_playing_immediate_stop()
+        raise asyncio.CancelledError from err
+
+
+async def consume_events_with_exc(
+    events_consumer: EventsConsumer, event_handler: EventHandler
+) -> None:
+    try:
+        await consume_events(events_consumer, event_handler)
+    except asyncio.CancelledError as err:
+        logger.info("Handling CancelledError")
+        raise asyncio.CancelledError from err
+
+
 async def consume_events(
     events_consumer: EventsConsumer, event_handler: EventHandler
 ) -> None:
@@ -27,41 +61,59 @@ async def consume_events(
         await asyncio.sleep(0.1)
 
 
-def main_tasks() -> list[asyncio.Task]:
+async def main_tasks() -> None:
     playing_manager = di[PlayingManager]
     break_observer = di[BreakObserver]
     playlist_events_consumer = di[PlaylistEventsConsumer]
     event_handler = di[EventHandler]
-
-    mp_task = asyncio.create_task(playing_manager.manage_playing())
-    ucb_task = asyncio.create_task(break_observer.update_current_break())
-    ce_task = asyncio.create_task(
-        consume_events(playlist_events_consumer, event_handler)
-    )
-    return [mp_task, ucb_task, ce_task]
+    try:
+        async with asyncio.TaskGroup() as tg:
+            mp_task = tg.create_task(manage_playing_with_exc(playing_manager))
+            ucb_task = tg.create_task(
+                update_current_break_with_exc(break_observer, playing_manager)
+            )
+            ce_task = tg.create_task(
+                consume_events_with_exc(playlist_events_consumer, event_handler)
+            )
+        tasks_results = [mp_task.result(), ucb_task.result(), ce_task.result()]
+        print(f"{tasks_results=}")
+    except* RuntimeError as ex:
+        logger.error(f"Caught RuntimeError")
+        logger.exception(ex)
+        raise SubTaskFailed from ex
+    except* Exception as ex:
+        logger.error(f"Caught Exception")
+        raise SubTaskFailed from ex
+    except* BaseException as ex:
+        logger.error(f"Caught BaseException")
+        logger.exception(ex)
+        raise SubTaskFailed from ex
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    tasks = main_tasks()
+    main_task = asyncio.create_task(main_tasks())
+    try:
+        await main_task
+    except SubTaskFailed as ex:
+        logger.error(f"Handling stop due to exception {ex=}")
 
     yield
-
     logger.info("Handling stop")
 
-    statuses_done = []
-    while True:
-        for task in tasks:
-            statuses_done.clear()
-            if task.done():
-                statuses_done.append(True)
-                continue
-            task.cancel()
-            statuses_done.append(False)
-        if not all(statuses_done):
-            await asyncio.sleep(0.1)
-        else:
-            break
+    # statuses_done = []
+    # while True:
+    #     for task in tasks:
+    #         statuses_done.clear()
+    #         if task.done():
+    #             statuses_done.append(True)
+    #             continue
+    #         task.cancel()
+    #         statuses_done.append(False)
+    #     if not all(statuses_done):
+    #         await asyncio.sleep(0.1)
+    #     else:
+    #         break
     logger.debug("The very end of the lifespan")
 
 
