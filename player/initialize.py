c72c0a6ec3fdbf2e5faf3c07b28504d403849350
Tomasz Wiśniewski <tomasz@wisniewski.app>
2025-02-25 20:50:55 +0100

    Add Dockerfile for player

---
+++ player/initialize.py
@@ -0,0 +1,9 @@
+from src.infrastructure.persistence import dbinit
+
+
+def main():
+    dbinit.main()
+
+
+if __name__ == "__main__":
+    main()
--- player/src/infrastructure/persistence/dbinit.py
+++ player/src/infrastructure/persistence/dbinit.py
@@ -2,5 +2,10 @@ from application.models.base import Base
 from infrastructure.persistence.database import engine
 from application.models.scheduled_tracks import ScheduledTrackModel  # noqa
 
-Base.metadata.create_all(bind=engine)
-print(Base.metadata)
+
+def main():
+    Base.metadata.create_all(bind=engine)
+
+
+if __name__ == "__main__":
+    main()
